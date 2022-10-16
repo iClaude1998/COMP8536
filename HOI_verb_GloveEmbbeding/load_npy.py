b36f8361c1597fbfd81dbb659f9ec619913c0696
Yunxiang Liu <u7191378@anu.edu.au>
2022-10-16 23:58:55 +1100

    add finetuned word vectors

--- HOI_verb_GloveEmbbeding/load_npy.py
+++ HOI_verb_GloveEmbbeding/load_npy.py
@@ -2,7 +2,7 @@
 Author: Yunxiang Liu u7191378@anu.edu.au
 Date: 2022-10-03 20:49:52
 LastEditors: Yunxiang Liu u7191378@anu.edu.au
-LastEditTime: 2022-10-12 17:17:11
+LastEditTime: 2022-10-16 23:58:11
 FilePath: \HoiTransformer\HOI_verb_GloveEmbbeding\load_npy.py
 Description: word embedding
 '''
@@ -17,29 +17,34 @@ if __name__ == "__main__":
 
     '''Load two .npy files as one list and one numpy array'''
     root_dir = 'HOI_verb_GloveEmbbeding'
-    wordsList = np.load(f'{root_dir}/HOI_Verb_wordsList.npy')
-    wordsList = wordsList.tolist()  # Originally loaded as numpy array
-    wordVectors = np.load(f'{root_dir}/HOI_Verb_wordVectors.npy')
-    print(wordVectors.shape)
+    # wordsList = np.load(f'{root_dir}/HOI_Verb_wordsList.npy')
+    # wordsList = wordsList.tolist()  # Originally loaded as numpy array
+    wordVectors = np.load(f'{root_dir}/finetuned_HOI_Verb_wordVectors_normed.npy')
+    print(wordVectors)
+    # norm = np.linalg.norm(wordVectors, axis=1)
+    # wordVectors = wordVectors / norm.reshape(-1, 1)
+    # print(wordVectors)
+    # np.save('HOI_verb_GloveEmbbeding/finetuned_HOI_Verb_wordVectors_normed.npy', wordVectors)
 
-    '''one example'''
-    theIndex = wordsList.index('adjust')
-    print(wordsList)
-    # print(wordVectors.shape)
-    # print(theIndex)
-    # print(wordVectors[theIndex])
 
-    '''if you like, you can change them into a dictionary: key-value == word-vector'''
-    embedding_dict={}
-    # print(hoi_interaction_names)
-    for i in range(0, len(wordsList)):
-        # if 'lie-on' in wordsList[i]:
-        #     print(wordsList[i])
-        embedding_dict[wordsList[i]] = wordVectors[i].tolist()
-    # embedding_dict['__background__'] = embedding_dict.pop('no_interaction')
-    json_str = json.dumps(embedding_dict)
-    with open('./data/hico/word_embeddings.json', 'w') as jf:
-        jf.write(json_str)
+    # '''one example'''
+    # theIndex = wordsList.index('adjust')
+    # print(wordsList)
+    # # print(wordVectors.shape)
+    # # print(theIndex)
+    # # print(wordVectors[theIndex])
+
+    # '''if you like, you can change them into a dictionary: key-value == word-vector'''
+    # embedding_dict={}
+    # # print(hoi_interaction_names)
+    # for i in range(0, len(wordsList)):
+    #     # if 'lie-on' in wordsList[i]:
+    #     #     print(wordsList[i])
+    #     embedding_dict[wordsList[i]] = wordVectors[i].tolist()
+    # # embedding_dict['__background__'] = embedding_dict.pop('no_interaction')
+    # json_str = json.dumps(embedding_dict)
+    # with open('./data/hico/word_embeddings.json', 'w') as jf:
+    #     jf.write(json_str)
 
     
 
